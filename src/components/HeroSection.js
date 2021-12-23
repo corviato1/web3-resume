c82946030603c1fcab6b3ae25b5ba2bbb14d4665
corviato1 <44780076+corviato1@users.noreply.github.com>
2021-12-23 13:22:11 -0800

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import CardItem from "./CardItem";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1><a href="https://ipfs.io/#why" target="_blank">Web3</a> Surfer & Agricultural Tinkerer </h1>
       <p><a href="https://ipfs.io/#how" target="_blank">Web3</a> supporter. Agricultural Consultant.  Cryptocurrency Miner.  Dapp Developer.</p>
       <h3>Site Powered by IPFS and FLEEK</h3>

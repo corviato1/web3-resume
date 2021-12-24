fd7cb7d61f75827039b3b51e5159cd11cb7a854f
corviato1 <44780076+corviato1@users.noreply.github.com>
2021-12-24 08:05:14 -0700

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,7 +9,7 @@ function HeroSection() {
     <div className="hero-container">
       <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1><a href="https://ipfs.io/#why" target="_blank">Web3</a> Surfer & Agricultural Tinkerer </h1>
-      <p><a href="https://ipfs.io/#how" target="_blank">Web3</a> supporter. Agricultural Consultant.  Cryptocurrency Miner.  Dapp Developer.</p>
+      <p><a href="https://ipfs.io/#how" target="_blank">Web3</a> supporter. Agricultural Consultant.  Cryptocurrency Miner.  DApp Developer.</p>
       <h3>Site Powered by IPFS and FLEEK</h3>
       <img className="Mike" src="images/home.jpg" />
     </div>

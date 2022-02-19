7eb0da090069808d0dfab58e1de5891d3a00d305
corviato1 <44780076+corviato1@users.noreply.github.com>
2022-02-18 19:24:08 -0700

    Update CardItem6.js
    https://giphy.com/gifs/thesimpsons-the-simpsons-3x14-xT5LMzIK1AdZJ4cYW4

--- src/components/CardItem6.js
+++ src/components/CardItem6.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardItem5(props) {
+function CardItem6(props) {
   return (
     <>
       <li className="cards__item">
